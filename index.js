3ec8a2304ae2b59dba69c03c822d1d0ddec9b98f
DwayneL <dr2@ameritech.net>
2019-11-13 15:14:38 -0600

    add fs writeFile functionality. App will write answers from array to pdf file

--- index.js
+++ index.js
@@ -2,17 +2,32 @@ const fs = require('fs');
 const inquirer = require('inquirer')
 
 
+
 console.log('work')
 
 inquirer
     .prompt([
         {
-        type: "input",
-        message: "what's your favorite color?",
-        name: "choice",
+            type: "input",
+            message: "what's your favorite color?",
+            name: "choice"
+        },
+        {
+            type: "input",
+            message: "what is your github username?",
+            name: "userName"
         }
-       
+      
     ])
     .then(answers => {
-        console.log("your favorite color is " + answers.choice)
+        fs.writeFile("log.pdf", JSON.stringify(answers), (err) => {
+            if (err) {
+                return console.log(err);
+            }
+        })
+        
+            
+       
+     
+      
     })
