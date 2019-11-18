37b1e7af15ef08cb914d278071ce2b2f5544cb36
DwayneL <dr2@ameritech.net>
2019-11-17 19:01:30 -0600

    updated formatting

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const axios = require('axios');
 
 
 
+
 inquirer
     .prompt([
         {
@@ -28,23 +29,58 @@ inquirer
         axios
         .get(queryURL)
         .then(function(results){
-            console.log(results.data.id)
             fs.writeFile("profgen.pdf", queryURL, (err) => {
                 if (err) {
                     return console.log(err);
                 } else {
                     const doc = new pdf;
                     
+                    // makes rectangle same color as favorite color prompt. Defaults to white if no color
                     doc.rect(0, 0, 700, 800)
-                    .fillColor("black")
+                    .fillColor("white")
                     .fillAndStroke(answers.choice)
+                    // pulls answers from prompt and pipes to pdf file
                     doc.pipe(fs.createWriteStream('profgen.pdf'));
-                    doc.font('Times-Roman')
-                    doc.fontSize(18)
-                    doc.fillColor("black")
-                    .text(results.data.avatar_url)
-                    doc.text(JSON.stringify("Location: " + results.data.location))
-                    doc.text(JSON.stringify("URL: " + results.data.html_url))
+                    // links inquire/js output to pdf
+                    doc.fontSize(20)
+                    .fillColor("black")
+                    .text("I'm " + results.data.name + ".", {
+                        width: 500,
+                        align: 'center'   
+                    });
+                    doc.fontSize(10)
+                    .fillColor("black")
+                    .text("See my profile picture at " + JSON.stringify(results.data.avatar_url),{
+                        align: 'center'
+                    });
+                    
+                    doc.font("Times-Roman")
+                    .moveDown(2)
+                    .fontSize(14)
+                    .text("Location: " + JSON.stringify(results.data.location),{
+                        align: 'left',
+                        continued:true
+                    })
+                    .text("GitHub: " +JSON.stringify(results.data.html_url),{
+                        align: "right"
+                    });
+
+                    doc.font("Times-Roman")
+                    .moveDown(1)
+                    .fontSize(14)
+                    .text("Followers: " + JSON.stringify(results.data.followers),{
+                        align: 'left',
+                        continued:true
+                    })
+                    .text("Following: " + JSON.stringify(results.data.following),{
+                        align: "center",
+                        continued: true
+                    })
+                    .text("Repositories: " + JSON.stringify(results.data.public_repos),{
+                        align: "right"
+
+                    });
+
                     
                     doc.end();
                 }
